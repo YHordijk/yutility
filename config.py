6f4556c1df8848aa9d00834aa24bb82d50ce10b1
Yuman Hordijk <yumanhordijk@gmail.com>
2023-01-26 21:28:55 +0100

    Added new Config class for loading and saving config files

--- config.py
+++ config.py
@@ -1,12 +1,24 @@
-from yutility import paths
+class Config:
+    def __init__(self, path):
+        self.data = {}
+        self.path = path
+        self.read()
 
-_config = {}
-with open(paths.config) as conf:
-    lines = [line.strip() for line in conf.readlines()]
+    def read(self):
+        with open(self.path) as conf:
+            lines = [line.strip() for line in conf.readlines()]
 
-for line in lines:
-    _config[line.split('=')[0]] = line.split('=')[1]
+        for line in lines:
+            self.data[line.split('=')[0]] = line.split('=')[1]
+
+    def save(self):
+        with open(self.path, 'w+') as conf:
+            for key, val in self.data.items():
+                conf.write(f'{key} = {val}\n')
 
 
-def get(key):
-    return _config.get(key)
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
