48d5f297754eef69a83d025a73ecc418d66803dc
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-01-30 16:04:08 +0100

    zip.py now works correctly, so far can archive directories

--- zip.py
+++ zip.py
@@ -1,20 +1,17 @@
 import tarfile
-from ychem.utility import paths
 import os
+from ychem.utility import paths
+from yutility import ensure_list
+import shutil
 
 j = os.path.join
 
 
-def zip_sps():
-    with tarfile.open(j(paths.sps_zip), 'w:gz') as tar:
-        tar.add(paths.sps, arcname=os.path.basename(paths.sps), recursive=True)
+def archive_dir(path, arcname=None, ftype='gz'):
+    assert ftype in ('gz', 'bz2', 'xz')
+    with tarfile.open(arcname, f'w|{ftype}') as tar:
+        tar.add(path, os.path.split(path)[1])
 
 
-def unzip_sps():
-    with tarfile.open(paths.sps_zip, 'r:gz') as tar:
-        print(os.path.dirname(paths.sps), paths.sps)
-        tar.extractall(path=os.path.dirname(paths.sps))
-
-
-# zip_sps()
-unzip_sps()
+if __name__ == '__main__':
+    archive_dir(paths.database, arcname=paths.database + '.tar.gz')
