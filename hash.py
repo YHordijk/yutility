1893da791ecd5c630c2eb3e67ad100f745f7baaa
Yuman Hordijk <yumanhordijk@gmail.com>
2023-02-27 11:22:36 +0100

    Added docstring

--- hash.py
+++ hash.py
@@ -3,8 +3,13 @@ import json
 import jsonpickle
 
 
-def hash(obj, use_json=True):
-    if use_json:
+def hash(obj):
+    '''
+    Hash any python object using SHA-256. If the object is a dictionary use JSON to
+    put it in a standard format. If there are non-picklable objects
+    use jsonpickle to pickle them anyway.
+    '''
+    if isinstance(obj, dict):
         try:
             obj = json.dumps(obj, sort_keys=True, indent=4)
         except:
