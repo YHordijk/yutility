7a151c24de10ab983f747786857623e31994f2b2
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-05-07 00:13:40 +0200

    Added Orbitals class and load function, for loading both MOs and SFOs for a single calculation

--- orbitals/__init__.py
+++ orbitals/__init__.py
@@ -1,1 +1,20 @@
 from yutility.orbitals import sfo, mo, info
+from scm import plams
+
+
+class Orbitals:
+    def __init__(self, path):
+        self.reader = plams.KFReader(path)
+        self.mos = mo.MOs(reader=self.reader)
+        self.sfos = sfo.SFOs(reader=self.reader)
+
+
+def load(path):
+    return Orbitals(path)
+
+
+if __name__ == '__main__':
+    p = '../test/orbitals/rkf/methyl.rkf'
+    orbs = Orbitals(p)
+    print(orbs.mos.mos)
+    print(orbs.sfos.sfos)
