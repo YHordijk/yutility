d4ca4a4fcd4c64c27a67e5689890af2a4b1e77e8
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-05-07 00:49:19 +0200

    Added calculation of mulliken contribution of an sfo to an mo

--- orbitals/__init__.py
+++ orbitals/__init__.py
@@ -1,5 +1,6 @@
 from yutility.orbitals import sfo, mo, info
 from scm import plams
+import numpy as np
 
 
 class Orbitals:
@@ -8,13 +9,35 @@ class Orbitals:
         self.mos = mo.MOs(reader=self.reader)
         self.sfos = sfo.SFOs(reader=self.reader)
 
+    def mulliken_contribution(self, sfo, mo):
+        if sfo.symmetry != mo.symmetry:
+            return 0
+
+        if sfo.spin != mo.spin:
+            return 0
+
+        n_i = sfo.occupation
+        c_ie = np.array(mo.coeffs)
+        c_iu = mo @ sfo
+        S_ue = np.array(sfo.overlaps)
+
+        return n_i * np.sum(c_ie*c_iu*S_ue)
+
 
 def load(path):
     return Orbitals(path)
 
 
 if __name__ == '__main__':
-    p = '../test/orbitals/rkf/methyl.rkf'
+    from yutility import ensure_list
+
+    p = '../test/orbitals/rkf/methyl_unrestricted_frags_symm.rkf'
     orbs = Orbitals(p)
-    print(orbs.mos.mos)
-    print(orbs.sfos.sfos)
+    mos = ensure_list(orbs.mos['2AA1_B'])
+    sfos = orbs.sfos.sfos
+
+    for mo_ in mos:
+        for sfo_ in sfos:
+            if orbs.mulliken_contribution(sfo_, mo_) == 0:
+                continue
+            print(mo_, sfo_, f'{orbs.mulliken_contribution(sfo_, mo_):.3%}')
