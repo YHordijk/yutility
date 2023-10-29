9e6c73f2e41c51bed6efdf9761c7f22ecdbed358
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-10-29 10:36:16 +0100

    Added a class containing colors for CLI

--- log.py
+++ log.py
@@ -392,6 +392,18 @@ def warn(txt):
     boxed_text(txt, double_edge=True, title='Warning', title_align='center')
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 if __name__ == '__main__':
     boxed_text('testing, 1, 2, 3, 4, 5, 6, 7, 8\nsecond row, 1, 2, 3, 4, 5, 6\n...\n...\n\n...\n...\nLast row here', title='ReactionRunner')
     mat = np.arange(3 * 3).reshape(3, 3)
