ffb3172a20c9ef7890d9eeb7e71b1e5b2a9eb616
Yuman Hordijk <yumanhordijk@gmail.com>
2024-01-09 16:33:22 +0100

    testing out writing multiple line values into excel

--- database.py
+++ database.py
@@ -214,8 +214,15 @@ class DBSelectResult:
         # first write the data
         for j, data in enumerate(self.data):
             for k, x in enumerate(data):
-                cell = sheet.cell(row=j+2, column=k+1, value=x)
-                cell.alignment = xl.styles.Alignment(wrapText=True)
+                if '\n' in x:
+                    s = ''
+                    for line in x.splitlines():
+                        s += f"""
+{line}"""           
+                    cell = sheet.cell(row=j+2, column=k+1, value=s)
+                    cell.alignment = xl.styles.Alignment(wrapText=True)
+                else:
+                    cell = sheet.cell(row=j+2, column=k+1, value=x)
 
         # write column headers
         for i, column in enumerate(self.columns):
