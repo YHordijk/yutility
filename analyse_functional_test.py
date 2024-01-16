292af98fea63c66cc569bd4a3dbcd47d37c5d84f
Hordijk <yhk800@login3.cm.cluster>
2024-01-17 00:27:23 +0100

    updated analysis of functional

--- analyse_functional_test.py
+++ analyse_functional_test.py
@@ -6,8 +6,8 @@ calc_dir = 'tmp/functional_test'
 for d in os.listdir(calc_dir):
     res = results.read(os.path.join(calc_dir, d))
     status = res.status.code
-    emoji = {'R': log.Emojis.wait, 'PD': log.Emojis.sleep, 'S': log.Emojis.good, 'F': log.Emojis.fail, 'W': log.Emojis.warning, 'U': log.Emojis.question}.get(status, '') + ' '
-    data.append((d.split('.')[0], emoji + res.status.name, '{: >6.2f}'.format(res.timing.total or 0), d.split('.')[1]))
+    emoji = {'R': log.Emojis.wait, 'PD': log.Emojis.sleep, 'S': log.Emojis.good, 'F': log.Emojis.fail, 'W': log.Emojis.warning + '\t', 'U': log.Emojis.question}.get(status, '')
+    data.append((emoji, d.split('.')[0], res.status.name, '{: >6.2f}'.format(res.timing.total or 0), d.split('.')[1]))
 
-data = sorted(data, key=lambda x: int(x[0]))
-log.table(data, ['ID', 'STATUS', 'TIMING (s)', 'FUNCTIONAL'])
+data = sorted(data, key=lambda x: int(x[1]))
+log.table(data, ['', 'ID', 'STATUS', 'TIMING (s)', 'FUNCTIONAL'])
